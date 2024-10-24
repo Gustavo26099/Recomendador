ff2a80d85b629560273ef751080a07676cca69bd
Gustavo26099 <161737236+Gustavo26099@users.noreply.github.com>
2024-10-24 11:26:03 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,12 +2,26 @@
 // Guardiões da galáxia, 12, fantasia, aventura, comédia, ficção cientifica
 // minha mãe é uma peça2, 12, comédia
 // homem-aranha sem volta para casa, 12, ação, fantasia, ficção cientifica, super-herói
-// homem-formiga e a vespa, 12, ação, aventura, ficção cientifica, super-herói
+// homem-formiga e a vespa, 12, ação, aventura, ficção cientifica, super-heróifunction setup() {
+
+
+
+
+
+
 function setup() {
-  createCanvas(400, 400);
+createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+  let idade = 15; 
+  let recomendacao = geraRecomendacao(idade);
+    text(recomendacao, width/2, height/2);
 }
+
+function geraRecomendacao(idade) {
+    if(idade >= 10) {
+
+
 
